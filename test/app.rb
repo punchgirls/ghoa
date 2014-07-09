61ffc929b179ed6c8bdddecad619fd26620c9237
maynkj <mayn.kjaer@gmail.com>
2014-07-09 13:19:22 -0300

    Initialize Ghoa.

--- test/app.rb
+++ test/app.rb
@@ -1,10 +1,12 @@
 require "cuba"
-require "requests"
+require_relative "../lib/ghoa"
 
 Cuba.define do
+  ghoa = Ghoa.new("client_id", "client_secret")
+
   on "github_oauth" do
     on param("code") do |code|
-      access_token = Ghoa.fetch_access_token(code)
+      access_token = ghoa.fetch_access_token(code)
 
       on access_token.nil? do
         res.status = 401
@@ -16,20 +18,7 @@ Cuba.define do
     end
 
     on get, root do
-      res.redirect Ghoa.authorize
-    end
-  end
-
-  on "login" do
-    user = JSON.parse((Requests.request("GET", "https://api.github.com/user",
-      params: { access_token: access_token })).body)
-
-    on user.nil? do
-      res.status = 403
-    end
-
-    on default do
-      res.write user
+      res.redirect ghoa.authorize
     end
   end
 end
