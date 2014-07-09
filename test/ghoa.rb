8b7e8293f34aa633740bca4625b139fd3138d322
maynkj <mayn.kjaer@gmail.com>
2014-07-09 13:22:45 -0300

    Add test.

--- test/ghoa.rb
+++ test/ghoa.rb
@@ -1,31 +1,16 @@
+require "cuba/test"
 require_relative "app"
 
-# Redefine fetch_user method to fake GitHub response
-
-module Ghoa
-  def fetch_user(access_token)
-    return { "id"=>"123456", :username=>"johndoe",
-     :name=>"John Doe", :email=>"johndoe@mail.com" }
+class Ghoa
+  def fetch_access_token(code)
+    return "078e5ae742d852d5b96723772469fbb4f65acd82"
   end
 end
 
 scope do
-  test "should redirect to GitHub login" do
-    get "/github_oauth"
+  test "get GitHub access token" do
+    get "/github_oauth?code=cfe70752888bb01f6c1a"
 
     assert_equal 302, last_response.status
   end
-
-  # test "should inform User of successful login" do
-  #   post "/github_login/117263273765215762"
-
-  #   post "/confirm", { developer: { name: "John Doe",
-  #     email: "johndoe@mail.com" }}
-
-  #   puts last_response.body
-
-  #   follow_redirect!
-
-  #   assert last_response.body["You have successfully logged in!"]
-  # end
 end
