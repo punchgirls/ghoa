4f3c14cdd7a533425c6b88052e05f446fa02be23
maynkj <mayn.kjaer@gmail.com>
2014-08-12 16:39:53 -0300

    Add test application.

--- test/app.rb
+++ test/app.rb
@@ -1,24 +1,29 @@
 require "cuba"
 require_relative "../lib/ghoa"
 
+CLIENT_ID = ENV.fetch("CLIENT_ID")
+CLIENT_SECRET = ENV.fetch("CLIENT_SECRET")
+
 Cuba.define do
-  ghoa = Ghoa.new("client_id", "client_secret")
+  ghoa = Ghoa.new(CLIENT_ID, CLIENT_SECRET)
 
   on "github_oauth" do
     on param("code") do |code|
       access_token = ghoa.fetch_access_token(code)
 
-      on access_token.nil? do
-        res.status = 401
+      on access_token != nil do
+        user = ghoa.fetch_user(access_token)
+
+        res.write JSON.dump(user)
       end
 
       on default do
-        res.status = 302
+        res.write "An error occured!"
       end
     end
 
     on get, root do
-      res.redirect ghoa.authorize
+      res.redirect ghoa.authorize_url
     end
   end
 end
