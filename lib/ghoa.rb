b0a111107437b415fd2a27b185d9e337cdf0dfd5
maynkj <mayn.kjaer@gmail.com>
2014-08-12 14:33:59 -0300

    Refactor.

--- lib/ghoa.rb
+++ lib/ghoa.rb
@@ -1,22 +1,33 @@
 require "requests"
 
 class Ghoa
+  AUTHORIZE_URL = "https://github.com/login/oauth/authorize?client_id=%s"
+  ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token"
+  USER_URL = "https://api.github.com/user"
+
   def initialize(client_id, client_secret)
     @client_id = client_id
     @client_secret = client_secret
   end
 
-  def authorize
-    return "https://github.com/login/oauth/authorize?client_id=#{ @client_id }"
+  def authorize_url
+    return sprintf(AUTHORIZE_URL, @client_id)
   end
 
   def fetch_access_token(code)
-    response = Requests.request("POST", "https://github.com/login/oauth/access_token",
+    response = Requests.request("POST", ACCESS_TOKEN_URL,
       data: { client_id: @client_id,
               client_secret: @client_secret,
               code: code },
       headers: { "Accept" => "application/json" })
 
-    return JSON.parse(response.body)["access_token"]
+    return response.json["access_token"]
+  end
+
+  def fetch_user(access_token)
+    params = { access_token: access_token }
+    response = Requests.request("GET", USER_URL, params: params)
+
+    return response.json
   end
 end
